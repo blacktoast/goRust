58860aa9beb63313ad71bf3173c2014a61e99894
토스트 <dbstjd100@naver.com>
2021-09-24 05:44:10 +0000

    replit 테스트 커밋이 안되왜

--- guessing/src/main.rs
+++ guessing/src/main.rs
@@ -23,7 +23,6 @@ fn main() {
             .expect("Please type a number!");
 
         println!("You guessed: {}", guess);
-
         match guess.cmp(&secret_number) {
             Ordering::Less    => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
