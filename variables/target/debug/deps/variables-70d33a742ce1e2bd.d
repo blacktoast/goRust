e05b6cb1012f7582370cef236e386d445f6cc208
토스트 <dbstjd100@naver.com>
2021-09-24 06:01:35 +0000

    variables 폴더 생성 데이터 타입 공부중

---
+++ variables/target/debug/deps/variables-70d33a742ce1e2bd.d
@@ -0,0 +1,5 @@
+/home/runner/goRust/variables/target/debug/deps/variables-70d33a742ce1e2bd: src/main.rs
+
+/home/runner/goRust/variables/target/debug/deps/variables-70d33a742ce1e2bd.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/runner/goRust/variables/target/debug/variables: /home/runner/goRust/variables/src/main.rs
