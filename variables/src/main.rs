fa388b6d7fdd6506d24c4336963008c3a2a5f087
blacktoast <dbstjd100@naver.com>
2021-09-24 06:22:17 +0000

    test -delete varialbed

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,13 +1,3 @@
 fn main() {
-  let x= 5;
-    println!("The value of x is: {}", x);
-  let  x=x+6;
-  println!("the value of xis {}",x );
-  let spaces = "   ";
-  //let spaces = spaces.len();
-  println!("test spaces {}", spaces);
-  let guess: u32 = "42".parse().expect("Not a number!");
-  let t= 24/2.5;
-  println!("what is t {}",t);
-
+    println!("Hello, world!");
 }
--- variables/target/debug/deps/variables-f2b9eb47c1bc8fa2.d
+++
@@ -1,5 +0,0 @@
-/home/runner/goRust/variables/target/debug/deps/variables-f2b9eb47c1bc8fa2.rmeta: src/main.rs
-
-/home/runner/goRust/variables/target/debug/deps/variables-f2b9eb47c1bc8fa2.d: src/main.rs
-
-src/main.rs:
