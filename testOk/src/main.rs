2fee8b6a611ec126491ede4a9bcd94d4cb943999
blacktoast <dbstjd100@naver.com>
2021-09-25 14:27:57 +0900

    재밌군

--- testOk/src/main.rs
+++ testOk/src/main.rs
@@ -1,8 +1,12 @@
 
 
 fn main() {
-    println!("Hello, world!");
-    println!("tets");
+    let a = [1, 2, 3, 4, 5];
+    let index = 10;
+
+    let element = a[index];
+
+    println!("The value of element is: {}", element);
 
 }
 
